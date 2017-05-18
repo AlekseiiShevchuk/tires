e0ca6c67e9e9d0e9c04d3df746725fb0880cb8f7
StanislavPisotskyi <stas27071992@i.ua>
2017-05-18 16:56:02 +0300

    add address account settings

---
+++ resources/views/account_address/account.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.index')
+
+@section('content')
+    <h3 class="page-title">@lang('quickadmin.users.title')</h3>
+    
+    {!! Form::model($user, ['method' => 'PUT', 'route' => ['account-address.update', $user->id]]) !!}
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            DINE PERSONLIGE OPLYSNINGER
+        </div>
+
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    {!! Form::label('address', 'Address*', ['class' => 'control-label']) !!}
+                    {!! Form::text('address', old('address'), ['class' => 'form-control', 'placeholder' => '', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('address'))
+                        <p class="help-block">
+                            {{ $errors->first('address') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    {!! Form::label('phone', 'Phone*', ['class' => 'control-label']) !!}
+                    {!! Form::text('phone', old('phone'), ['class' => 'form-control', 'placeholder' => '', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('phone'))
+                        <p class="help-block">
+                            {{ $errors->first('phone') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    {!! Form::label('password', 'Password', ['class' => 'control-label']) !!}
+                    {!! Form::password('password', ['class' => 'form-control', 'placeholder' => '', 'required' => '']) !!}
+                    <p class="help-block"></p>
+                    @if($errors->has('password'))
+                        <p class="help-block">
+                            {{ $errors->first('password') }}
+                        </p>
+                    @endif
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    {!! Form::submit(trans('quickadmin.qa_update'), ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+@stop
+
--- resources/views/account_settings/index.blade.php
+++ resources/views/account_settings/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <ul>
-        <li><a href="#">Address settings</a></li>
+        <li><a href="{{ action('AccountAddressController@index') }}">Address settings</a></li>
         <li><a href="{{ action('AccountController@index') }}">Account settings</a></li>
     </ul>
 @stop
