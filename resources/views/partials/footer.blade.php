61a5289edfd7b47628c83b2d5f52fc71d83d74dd
Sergey <sergey.priakhin@gmail.com>
2017-05-23 19:39:15 +0300

    fix v3

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,63 +1,83 @@
-    <div class="container-fluid small-footer">
+  <div class="small-footer">
+      <div class="container">
         <div class="row">
-            <div class="col-md-6 small-footer-wrap">
-                <span class="content-footer-span left-margin">Nyhedsbrev</span>
-                <div class="small-footer-wrap-input">
+          <div class="col-md-12">
+            <div class="pull-left">
+                <span class="content-footer-span">Nyhedsbrev</span>
+                <form class="small-footer-wrap-input">
                     <input class="input-footer" type="text" placeholder="Din e-mail">
-                    <div class="input-footer-btn">></div>
-                </div>
+                    <button type="submit" class="btn btn-default button input-footer-btn">
+                      <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    </button>
+                </form>
             </div>
-            <div class="col-md-3 col-md-offset-2 small-footer-wrap-2">
+            <div class="pull-right">
                 <span class="content-footer-span">Følg os</span>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid footer">
-        <div class="row">
-            <div class="col-md-1 footer-wrap">
-                <ul class="footer-ul">
-                    <li class="first-li">Kategorier</li>
-                    <li class="li"><a href="#">Sommerdæk</a></li>
-                    <li class="li"><a href="#">Vinterdæk</a></li>
-                    <li class="li"><a href="#">Helårsdæk</a></li>
-                </ul>
-            </div>
-            <div class="col-md-1 col-md-offset-1 footer-wrap">
-                <ul class="footer-ul">
-                    <li class="first-li">Information</li>
-                    <li class="li"><a href="#">Nyheder</a></li>
-                    <li class="li"><a href="#">Kontakt os</a></li>
-                    <li class="li"><a href="#">Handelsbetingelser</a></li>
-                    <li class="li"><a href="#">Om Os</a></li>
-                </ul>
-            </div>
-            <div class="col-md-2 col-md-offset-1 footer-wrap">
-                <ul class="footer-ul">
-                    <li class="first-li">Min konto</li>
-                    <li class="li"><a href="#">Mine ordrer</a></li>
-                    <li class="li"><a href="#">Mine kreditnotaer</a></li>
-                    <li class="li"><a href="#">Mine adresser</a></li>
-                    <li class="li"><a href="#">Mine oplysninger</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3 col-md-offset-2">
-                <div class="big-footer-wrap-info">
-                    <ul class="footer-ul">
-                        <li class="first-li">Butiksinformation</li>
-                        <li class="li">
-                            <span class="light-grey-color">Fire Dæk, Smedeland 7, st. 2600 Glostrup</span>
-                        </li>
-                        <li class="li">
-                            <span class="light-grey-color">Ring til os:</span>
-                            <span class="white-color-bold">32203218</span>
-                        </li>
-                        <li class="li">
-                            <span class="light-grey-color">E-mail:</span>
-                            <span class="white-color-bold">info@fire-daek.dk</span>
-                        </li>
-                    </ul>
+                <div class="small-footer__social">
+                  <a href="#">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                  </a>
+                  <a href="#">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                  </a>
+                  <a href="#">
+                    <i class="fa fa-rss" aria-hidden="true"></i>
+                  </a>
+                  <a href="#">
+                    <i class="fa fa-google-plus" aria-hidden="true"></i>
+                  </a>
                 </div>
             </div>
         </div>
     </div>
+    </div>
+  </div>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-2 footer-wrap">
+                <ul class="footer-list">
+                    <li class="footer-list__caption">Kategorier</li>
+                    <li class="footer-list__item"><a href="#">Sommerdæk</a></li>
+                    <li class="footer-list__item"><a href="#">Vinterdæk</a></li>
+                    <li class="footer-list__item"><a href="#">Helårsdæk</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 footer-wrap">
+                <ul class="footer-list">
+                    <li class="footer-list__caption">Information</li>
+                    <li class="footer-list__item"><a href="#">Nyheder</a></li>
+                    <li class="footer-list__item"><a href="#">Kontakt os</a></li>
+                    <li class="footer-list__item"><a href="#">Handelsbetingelser</a></li>
+                    <li class="footer-list__item"><a href="#">Om Os</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 footer-wrap">
+                <ul class="footer-list">
+                    <li class="footer-list__caption">Min konto</li>
+                    <li class="footer-list__item"><a href="#">Mine ordrer</a></li>
+                    <li class="footer-list__item"><a href="#">Mine kreditnotaer</a></li>
+                    <li class="footer-list__item"><a href="#">Mine adresser</a></li>
+                    <li class="footer-list__item"><a href="#">Mine oplysninger</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 footer-border-left footer-wrap">
+                <ul class="footer-list">
+                    <li class="footer-list__caption">Butiksinformation</li>
+                    <li class="footer-list__item">
+                        <span class="light-grey-color">Fire Dæk, Smedeland 7, st. 2600 Glostrup</span>
+                    </li>
+                    <li class="footer-list__item">
+                        <span class="light-grey-color">Ring til os:</span>
+                        <span class="white-color-bold">32203218</span>
+                    </li>
+                    <li class="footer-list__item">
+                        <span class="light-grey-color">E-mail:</span>
+                        <span class="white-color-bold">info@fire-daek.dk</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </div>
+  </footer>
