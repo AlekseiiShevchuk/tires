86b1e0a63eb758dcdc728cb4b866fb835fd490a1
Sergey <sergey.priakhin@gmail.com>
2017-05-23 18:20:15 +0300

    fix v2

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -4,9 +4,11 @@
       <div class="col-md-4">
           <img src="{{ asset('index-page/fire-daek-logo-1490775043.jpg') }}" class="header__logo">
       </div>
-      <div class="col-md-4 search-content">
-          <input class="search" type="text" placeholder="Søg">
-          <button class="search-btn"><i class="glyphicon glyphicon-search"></i></button>
+      <div class="col-md-4">
+        <div class="search-content">
+          <input class="search-content__input" type="text" placeholder="Søg">
+          <button class="search-content__btn"><i class="glyphicon glyphicon-search"></i></button>
+        </div>
       </div>
       <div class="col-md-3 col-md-offset-1">
           <div class="shopping-cart">
