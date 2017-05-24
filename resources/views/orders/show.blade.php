a9df6819949a523d1136f1b5211ae0937b3aab86
StanislavPisotskyi <stas27071992@i.ua>
2017-05-24 10:35:23 +0300

    order many to many

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -23,8 +23,36 @@
                     <p><b>First name: </b>{{$order->user->first_name}}</p>
                     <p><b>Last name: </b>{{$order->user->last_name}}</p>
                     <p><b>Name: </b>{{$order->user->name}}</p>
+                    <p><b>Email: </b>{{$order->user->email}}</p>
                     <p><b>Address: </b>{{$order->user->address}}</p>
                     <p><b>Phone: </b>{{$order->user->phone}}</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Brand</th>
+                                <th>Size</th>
+                                <th>Price</th>
+                                <th>Special Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($order->tires as $tire)
+                                <tr>
+                                    <td>{{$tire->name}}</td>
+                                    <td>{{$tire->brand->name}}</td>
+                                    <td>{{$tire->size->size}}</td>
+                                    <td>{{$tire->price}}</td>
+                                    <td>{{!is_null($tire->special_price) ? $tire->special_price : '-'}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
